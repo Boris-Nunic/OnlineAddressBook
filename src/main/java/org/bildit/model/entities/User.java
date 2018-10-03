4f68c7962d73398a9079676f34edc4fc9c0badc9
Boris-nunic <boris.nunic88@gmail.com>
2018-10-03 10:17:02 +0200

    PersonalInfo.java and Address.java added to src/main/java/org/bildit/model/entities/, implementation added for User.java

--- src/main/java/org/bildit/model/entities/User.java
+++ src/main/java/org/bildit/model/entities/User.java
@@ -2,4 +2,53 @@ package org.bildit.model.entities;
 
 public class User {
 
+	private Integer id;
+	private PersonalInfo peronalInfo = new PersonalInfo();
+	private Address address = new Address();
+	private String password;
+	
+	public User() {
+		
+	}
+	
+	public User(String email, String password) {
+		this.getAddress().setEmail(email);
+		this.setPassword(password);
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public PersonalInfo getPeronalInfo() {
+		return peronalInfo;
+	}
+
+	public void setPeronalInfo(PersonalInfo peronalInfo) {
+		this.peronalInfo = peronalInfo;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
 }
