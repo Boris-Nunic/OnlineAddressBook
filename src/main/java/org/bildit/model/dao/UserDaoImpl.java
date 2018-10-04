08a27643834e83282e34acf14b7544d3562a4f14
Boris-nunic <boris.nunic88@gmail.com>
2018-10-04 22:16:56 +0200

    Implementation added for editUser(User user) in org.bildit.dao.UserDaoImpl.java; info.jsp removed

--- src/main/java/org/bildit/model/dao/UserDaoImpl.java
+++ src/main/java/org/bildit/model/dao/UserDaoImpl.java
@@ -13,14 +13,14 @@ public class UserDaoImpl implements UserDaoInterface {
 	Connection connection = ConnectionManager.getInstance().getConnection();
 
 	@Override
-	public int addUser(User user) throws SQLException{
-		
-		String query = "INSERT INTO users(password, first_name, surname, email," + 
-				" phone_number, dob, street_address, city, country ) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
-		
+	public int addUser(User user) throws SQLException {
+
+		String query = "INSERT INTO users(password, first_name, surname, email,"
+				+ " phone_number, dob, street_address, city, country ) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
 		int rowsAffected = 0;
-		try(PreparedStatement ps = connection.prepareStatement(query);) {
-			
+		try (PreparedStatement ps = connection.prepareStatement(query);) {
+
 			ps.setString(1, user.getPassword());
 			ps.setString(2, user.getPersonalInfo().getFirstName());
 			ps.setString(3, user.getPersonalInfo().getSurname());
@@ -30,25 +30,25 @@ public class UserDaoImpl implements UserDaoInterface {
 			ps.setString(7, user.getAddress().getStreetAddress());
 			ps.setString(8, user.getAddress().getCity());
 			ps.setString(9, user.getAddress().getCountry());
-			
-			rowsAffected =  ps.executeUpdate();
-			
-		} 
-		
+
+			rowsAffected = ps.executeUpdate();
+
+		}
+
 		return rowsAffected;
 	}
 
 	@Override
 	public User getUser(String email) {
-		
+
 		String query = "SELECT * FROM users WHERE email=?";
 		User user = null;
-		
-		try{
+
+		try {
 			PreparedStatement ps = connection.prepareStatement(query);
 			ps.setString(1, email);
 			ResultSet rs = ps.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				user = new User(rs.getString("email"), rs.getString("password"));
 				user.setId(rs.getInt("user_id"));
 				user.getPersonalInfo().setFirstName(rs.getString("first_name"));
@@ -59,18 +59,39 @@ public class UserDaoImpl implements UserDaoInterface {
 				user.getAddress().setCountry(rs.getString("country"));
 				user.getAddress().setStreetAddress(rs.getString("street_address"));
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return user;
 	}
 
 	@Override
 	public int editUser(User user) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		int rowsAffected = 0;
+		String query = "UPDATE users SET first_name = ?, surname = ?, email = ?, dob = ?,"
+				+ " phone_number = ?, street_address = ?, city = ?, country = ? WHERE user_id = ?";
+		
+		try {
+			PreparedStatement ps = connection.prepareStatement(query);
+			ps.setString(1, user.getPersonalInfo().getFirstName());
+			ps.setString(2, user.getPersonalInfo().getSurname());
+			ps.setString(3, user.getAddress().getEmail());
+			ps.setDate(4, user.getPersonalInfo().getDob());
+			ps.setString(5, user.getPersonalInfo().getPhoneNumber());
+			ps.setString(6, user.getAddress().getStreetAddress());
+			ps.setString(7, user.getAddress().getCity());
+			ps.setString(8, user.getAddress().getCountry());
+			ps.setInt(9, user.getId());
+
+			rowsAffected = ps.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return rowsAffected;
 	}
 
 	@Override
