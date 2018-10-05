836c63a5a13c7bd0c3d1d96956f2e8125a4e9ca1
Boris-nunic <boris.nunic88@gmail.com>
2018-10-05 12:46:15 +0200

    Implementation adde to org.bildit.model.entities.Contact.java

--- src/main/java/org/bildit/model/entities/Contact.java
+++ src/main/java/org/bildit/model/entities/Contact.java
@@ -2,4 +2,28 @@ package org.bildit.model.entities;
 
 public class Contact {
 
+	private PersonalInfo personalInfo = new PersonalInfo();
+	private Address address = new Address();
+	
+	public Contact() {
+		
+	}
+
+	public PersonalInfo getPersonalInfo() {
+		return personalInfo;
+	}
+
+	public void setPersonalInfo(PersonalInfo personalInfo) {
+		this.personalInfo = personalInfo;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	
 }
