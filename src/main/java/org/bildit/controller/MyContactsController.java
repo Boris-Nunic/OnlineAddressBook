ed2d2e6bfddf38ed57796c491373e9e25b364f56
Boris-nunic <boris.nunic88@gmail.com>
2018-10-22 11:32:05 +0200

    MyContactsController.java modified, it now calls getAllContacts(Integer userId):ArrayList<Contact> method to get all contacts as ArrayList for specific user, then it sets that list as request attribute and sends it to MyContacts.jsp; Return type of getAllContacts(Integer userId) changed to ArrayList<Contact>

--- src/main/java/org/bildit/controller/MyContactsController.java
+++ src/main/java/org/bildit/controller/MyContactsController.java
@@ -1,10 +1,16 @@
 package org.bildit.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.bildit.model.entities.Contact;
+import org.bildit.model.entities.User;
+import org.bildit.model.service.ContactService;
 
 /**
  * Servlet implementation class MyContactsController
@@ -18,11 +24,9 @@ public class MyContactsController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-	}
-	
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		User user = (User)request.getSession().getAttribute("user");
+		ArrayList<Contact> contacts = ContactService.getAllContacts(user.getId());
+		request.setAttribute("allContacts", contacts);
 		request.getRequestDispatcher("myContacts.jsp").forward(request, response);
 	}
 
--- src/main/java/org/bildit/model/service/ContactService.java
+++ src/main/java/org/bildit/model/service/ContactService.java
@@ -1,32 +1,33 @@
 package org.bildit.model.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.bildit.model.dao.ContactDaoImpl;
 import org.bildit.model.entities.Contact;
 
 public class ContactService {
-	
+
 	private static ContactDaoImpl contactDao = new ContactDaoImpl();
-	
+
 	public static String addContact(Contact contact, Integer userId) {
-		
+
 		String message;
 		Integer rowsAffected = contactDao.addContact(contact, userId);
-		
-		if(rowsAffected != 1) {
+
+		if (rowsAffected != 1) {
 			message = "Somethig went wrong. Please try again";
 		}
-		
+
 		else {
 			message = "This contact was successfully added to your contacts";
 		}
 		return message;
 	}
-	
-	public static List<Contact> getAllContacts(Integer userId){
-		return null;
+
+	public static ArrayList<Contact> getAllContacts(Integer userId) {
+		ArrayList<Contact> contacts = (ArrayList<Contact>) contactDao.getAllContacts(userId);
+		return contacts;
 	}
-			
 
 }
