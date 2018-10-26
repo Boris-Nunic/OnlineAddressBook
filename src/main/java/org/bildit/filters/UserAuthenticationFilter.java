72329c85b1038b470587a5f93ca5fdc6b0b8b74b
Boris-nunic <boris.nunic88@gmail.com>
2018-10-26 11:49:46 +0200

    UserAutenticationFilter.java renamed to UserAuthenticationFilter.java; URL patterns added for this filter in web.xml, URL patterns icluded are /userProfile, /editProfile, /deleteProfile, /myContacts, /addContact

---
+++ src/main/java/org/bildit/filters/UserAuthenticationFilter.java
@@ -0,0 +1,59 @@
+package org.bildit.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.bildit.model.entities.User;
+
+/**
+ * Servlet Filter implementation class UserAutenticationFilter
+ */
+public class UserAuthenticationFilter implements Filter {
+
+	/**
+	 * Default constructor.
+	 */
+	public UserAuthenticationFilter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+		User user = (User) request.getSession().getAttribute("user");
+		if (user == null) {
+			response.sendRedirect("loginRegister");
+		}
+
+		else {
+			chain.doFilter(request, response);
+		}
+
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
