5356bbb4af46c588eb406158e9e81b9a0528d9cb
Boris-nunic <boris.nunic88@gmail.com>
2018-11-02 10:14:08 +0100

    org.bildit.controller.EditContactController.java aded to src/main/java/, this controller was configured in web.xml

---
+++ src/main/java/org/bildit/controller/EditContactController.java
@@ -0,0 +1,64 @@
+package org.bildit.controller;
+
+import java.io.IOException;
+import java.sql.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.bildit.model.entities.Contact;
+import org.bildit.model.service.ContactService;
+
+/**
+ * Servlet implementation class EditContactController
+ */
+public class EditContactController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String urlPattern = "editContact";
+		request.setAttribute("urlPattern", urlPattern);
+		request.getRequestDispatcher("addContact.jsp").forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String firstName = request.getParameter("firstName");
+		String surname = request.getParameter("surname");
+		String email = request.getParameter("email");
+		String phoneNumber = request.getParameter("phoneNumber");
+		String streetAddress = request.getParameter("streetAddress");
+		String city = request.getParameter("city");
+		String country = request.getParameter("country");
+		String dob = request.getParameter("dob");
+		Integer id = Integer.parseInt(request.getParameter("contactId"));
+		
+		Contact contact = new Contact();
+		
+		if(!dob.equals("")) {
+			contact.getPersonalInfo().setDob(Date.valueOf(dob));
+		}
+		
+		contact.setId(id);
+		contact.getPersonalInfo().setFirstName(firstName);
+		contact.getPersonalInfo().setSurname(surname);
+		contact.getPersonalInfo().setPhoneNumber(phoneNumber);
+		contact.getAddress().setEmail(email);
+		contact.getAddress().setStreetAddress(streetAddress);
+		contact.getAddress().setCity(city);
+		contact.getAddress().setCountry(country);
+		
+		String message = ContactService.editContact(contact);
+		
+		request.setAttribute("message", message);
+		request.getRequestDispatcher("myContacts").forward(request, response);
+	}
+
+}
