e36bec03d20eff4f5d8d41ca0b8b564a0ba419e6
Boris Nunic <boris.nunic88@gmail.com>
2019-01-24 14:52:37 +0100

    Configuretion for com.boris.controller.RegistrationFormController removed from web.xml, configuration for /view/registration.jsp added to web.xml. doGet method implemented in com.boris.RegistrationForm.Controller

--- src/main/java/com/boris/controller/RegistrationFormController.java
+++ src/main/java/com/boris/controller/RegistrationFormController.java
@@ -1,7 +1,11 @@
 package com.boris.controller;
 
 import java.io.IOException;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,12 +15,25 @@ import com.boris.model.service.UserService;
 /**
  * Servlet implementation class RegistrationFormController
  */
+@WebServlet(
+		urlPatterns = "/register",
+		initParams = @WebInitParam(name="registrationNav", value="home,login")
+		)
 public class RegistrationFormController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+	
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletConfig config = getServletConfig();
+		request.setAttribute("registrationNav", config.getInitParameter("registrationNav"));
+		request.getRequestDispatcher("registrationPage").forward(request, response);
+	}
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		String email = request.getParameter("email");
