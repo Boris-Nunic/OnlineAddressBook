a59e0f499e308a70348c20d6e2379be88ce34996
Boris Nunic <boris.nunic88@gmail.com>
2019-01-25 15:22:09 +0100

    com.boris.controller.MyContacts cotroller is now confugured with annotations, init params are set for this servlet, these parameters are sent with request. The control is forward to /contactsPage

--- src/main/java/com/boris/controller/MyContactsController.java
+++ src/main/java/com/boris/controller/MyContactsController.java
@@ -3,18 +3,21 @@ package com.boris.controller;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.boris.model.entities.Contact;
-import com.boris.model.entities.User;
 import com.boris.model.service.ContactService;
 
 /**
  * Servlet implementation class MyContactsController
  */
+@WebServlet(urlPatterns="/contacts", initParams=@WebInitParam(name="contactsNav", value="profile,logout"))
 public class MyContactsController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -24,13 +27,15 @@ public class MyContactsController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		ServletConfig config = getServletConfig();
+		request.setAttribute("nav", config.getInitParameter("contactsNav"));
 		Integer userId = (Integer)request.getSession().getAttribute("userId");
 		ArrayList<Contact> contacts = ContactService.getAllContacts(userId);
 		String message = (String)request.getSession().getAttribute("message");
 		request.getSession().removeAttribute("message");
 		request.setAttribute("message", message);
 		request.setAttribute("contacts", contacts);
-		request.getRequestDispatcher("myContacts.jsp").forward(request, response);
+		request.getRequestDispatcher("contactsPage").forward(request, response);
 	}
 	
 //	@Override
