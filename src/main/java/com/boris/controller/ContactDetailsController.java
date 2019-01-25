67e085cc823363ddf02966ff8194203e9ee45b90
Boris Nunic <boris.nunic88@gmail.com>
2019-01-25 20:42:12 +0100

    book-background.jpg added to src/main/webapp/img/; contactDetails.jsp added to src/main/webapp/view/, thid jsp is configured in web.xml; com.boris.controller.ContactDetailsController changed, it's configured with annotation, web.xml updated, request attribure contact renamed to bean

--- src/main/java/com/boris/controller/ContactDetailsController.java
+++ src/main/java/com/boris/controller/ContactDetailsController.java
@@ -1,18 +1,22 @@
 package com.boris.controller;
 
 import java.io.IOException;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.boris.model.entities.Contact;
-import com.boris.model.entities.User;
 import com.boris.model.service.ContactService;
 
 /**
  * Servlet implementation class ContactDetailsController
  */
+@WebServlet(urlPatterns = "/details", initParams = @WebInitParam(name = "contactDetailsNav", value = "edit,delete,contacts,profile"))
 public class ContactDetailsController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -22,10 +26,12 @@ public class ContactDetailsController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		Integer contactId =Integer.parseInt(request.getParameter("contactId"));
+		ServletConfig config = getServletConfig();
+		request.setAttribute("nav", config.getInitParameter("contactDetailsNav"));
+		Integer contactId = Integer.parseInt(request.getParameter("contactId"));
 		Integer userId = (Integer) request.getSession().getAttribute("userId");
 		Contact contact = ContactService.getContact(contactId, userId);
-		request.setAttribute("contact", contact);
-		request.getRequestDispatcher("contactDetails.jsp").forward(request, response);
+		request.setAttribute("bean", contact);
+		request.getRequestDispatcher("contactDetailsPage").forward(request, response);
 	}
 }
